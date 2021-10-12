d740e1671fdfc178e8fef00865bf5196ca4bd982
jefersonrl <jeferson.lima17@etec.sp.gov.br>
2021-10-12 12:11:54 -0300

    Adding image to the app

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.example.diceroller.R.id.luckyNumber
@@ -25,9 +26,16 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.Number)
+        val diceImage: ImageView = findViewById(R.id.imageView)
 
-        resultTextView.text = diceRoll.toString()
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
 
         val sortRoll = dice.roll()
         val sortTextView: TextView = findViewById(R.id.luckyNumber)
